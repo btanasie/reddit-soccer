46fed595ec4bce4166e0cf52e60148993718da1e
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2021-02-18 16:48:36 -0500

    Transfer collab files

---
+++ RedditCrawler.py
@@ -0,0 +1,29 @@
+import praw
+import pandas as pd
+
+reddit = praw.Reddit(client_id='d8FC4nZtpiV5tw', client_secret='Zqa3Hy5ePYBNbMGxX_zSyr_tiF3Z-A', user_agent='soccer_scraper')
+
+def GetPosts(soccer_subreddit):
+  # Get
+  posts = []
+  for post in soccer_subreddit:
+      # Get comments
+      comments = []
+      submission = reddit.submission(id=post.id)
+      submission.comments.replace_more(limit=0)
+      for comment in submission.comments.list():
+        #comments.append(comment.body.replace('\n', ' ').replace('\r', ' '))
+        # print(comment.created)
+        # print(comment.author)
+        posts.append([post.title, post.score, post.id, post.url, post.selftext, post.created, comment.body.replace('\n', ' ').replace('\r', ' '), comment.author, comment.created, comment.upvote, comment.downvote])
+
+  return pd.DataFrame(posts, columns=['ptitle', 'pscore', 'pid', 'purl', 'pbody', 'pcreated', 'comment', 'cauthor', 'ccreated', 'cupvote', 'cdownvote'])
+
+pre_soccer_subreddit = reddit.subreddit('soccer').search('flair:Pre Match Thread')
+pre_posts = GetPosts(pre_soccer_subreddit)
+
+pre_soccer_subreddit = reddit.subreddit('soccer').search('flair:Post Match Thread')
+post_posts = GetPosts(pre_soccer_subreddit)
+
+pre_posts.to_csv("./data/pre_soccer_extraction.csv",index=False)
+post_posts.to_csv("./data/post_soccer_extraction.csv",index=False)
