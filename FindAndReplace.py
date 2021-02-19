586e63991c1c3aa24b4580879b61b61bc16c88fc
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2021-02-18 20:26:12 -0500

    Add find & replace

---
+++ FindAndReplace.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+pre_df = pd.read_csv('./data/pre_soccer_extraction.csv')
+post_df = pd.read_csv('./data/post_soccer_extraction.csv')
+teams = pd.read_csv('./data/teams.csv')
+
+post_df['Comment'] = post_df['Comment'].str.lower()
+pre_df['Comment'] = post_df['Comment'].str.lower()
+teams['Other Name'] = teams['Other Name'].str.lower()
+
+
+def Replace(df, replace_list, key):
+  #for i in range(0,len(df)):
+  for (main_value, to_replace) in replace_list:
+    df[key].replace({to_replace: main_value}, regex=True,inplace=True)
+    #df[key][i] = df[key][i].replace(to_replace.lower(), main_value.lower())
+
+  return df
+
+
+post_df = Replace(post_df, teams, 'Comment')
+pre_df = Replace(pre_df, teams, 'Comment')
+
+pre_df.to_csv('./data/pre_soccer_replaced.csv')
+post_df.to_csv('./data/post_soccer_replaced.csv')
