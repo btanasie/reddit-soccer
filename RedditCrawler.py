d35a0a51dd20a678c4abc82ccd8a90cb7c764d3c
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2021-02-19 16:05:10 -0500

    Update crawler

--- RedditCrawler.py
+++ RedditCrawler.py
@@ -3,27 +3,36 @@ import pandas as pd
 
 reddit = praw.Reddit(client_id='d8FC4nZtpiV5tw', client_secret='Zqa3Hy5ePYBNbMGxX_zSyr_tiF3Z-A', user_agent='soccer_scraper')
 
-def GetPosts(soccer_subreddit):
-  # Get
-  posts = []
-  for post in soccer_subreddit:
-      # Get comments
-      comments = []
-      submission = reddit.submission(id=post.id)
-      submission.comments.replace_more(limit=None)
-      for comment in submission.comments.list():
-        #comments.append(comment.body.replace('\n', ' ').replace('\r', ' '))
-        # print(comment.created)
-        # print(comment.author)
-        posts.append([post.title, post.score, post.id, post.selftext, post.created, comment.body.replace('\n', ' ').replace('\r', ' '), comment.author, comment.created])
+def GetPosts(soccer_subreddit, timeline, limit, columns):
+    # Get
+    posts = []
+    index = 0
+    print(timeline)
+    for post in soccer_subreddit:
+        print(index)
+        # Get comments
+        submission = reddit.submission(id=post.id)
+        submission.comments.replace_more(limit=limit)
 
-  return pd.DataFrame(posts, columns=['ptitle', 'pscore', 'pid', 'pbody', 'pcreated', 'comment', 'cauthor', 'ccreated'])
+        if timeline == 'pre':
+            for comment in submission.comments.list():
+                posts.append([post.title, post.score, post.id, post.selftext, post.created, comment.body.replace('\n', ' ').replace('\r', ' '), comment.author, comment.created])
+        else:
+            posts.append([post.title, post.score, post.id, post.selftext])
+
+        index += 1
+    print('\n')
+    if timeline == 'pre':
+        return pd.DataFrame(posts, columns=columns)
+    else:
+        return pd.DataFrame(posts, columns=columns)
+
 
 pre_soccer_subreddit = reddit.subreddit('soccer').search('flair:Pre Match Thread')
-pre_posts = GetPosts(pre_soccer_subreddit)
+pre_posts = GetPosts(pre_soccer_subreddit, timeline='pre', limit=None, columns=['ptitle', 'pscore', 'pid', 'pbody', 'pcreated', 'comment', 'cauthor', 'ccreated'])
 
-pre_soccer_subreddit = reddit.subreddit('soccer').search('flair:Post Match Thread')
-post_posts = GetPosts(pre_soccer_subreddit)
+post_soccer_subreddit = reddit.subreddit('soccer').search('flair:Post Match Thread')
+post_posts = GetPosts(post_soccer_subreddit, timeline='post', limit=25, columns=['ptitle', 'pscore', 'pid', 'pbody'])
 
-pre_posts.to_csv("./data/pre_soccer_extraction.csv",index=False)
-post_posts.to_csv("./data/post_soccer_extraction.csv",index=False)
+pre_posts.to_csv("./data/pre_soccer_extraction.csv", index=False)
+post_posts.to_csv("./data/post_soccer_extraction.csv", index=False)
