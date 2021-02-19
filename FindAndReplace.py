218fc1cc042417f24a788530bd4f0091a2ad742d
Richard <rich.gao@hotmail.com>
2021-02-19 10:56:28 -0500

    changed replace function

--- FindAndReplace.py
+++ FindAndReplace.py
@@ -4,22 +4,22 @@ pre_df = pd.read_csv('./data/pre_soccer_extraction.csv')
 post_df = pd.read_csv('./data/post_soccer_extraction.csv')
 teams = pd.read_csv('./data/teams.csv')
 
-post_df['Comment'] = post_df['Comment'].str.lower()
-pre_df['Comment'] = post_df['Comment'].str.lower()
+post_df['comment'] = post_df['comment'].str.lower()
+pre_df['comment'] = post_df['comment'].str.lower()
 teams['Other Name'] = teams['Other Name'].str.lower()
 
 
 def Replace(df, replace_list, key):
   #for i in range(0,len(df)):
-  for (main_value, to_replace) in replace_list:
+  for (main_value, to_replace) in replace_list.values:
     df[key].replace({to_replace: main_value}, regex=True,inplace=True)
     #df[key][i] = df[key][i].replace(to_replace.lower(), main_value.lower())
 
   return df
 
 
-post_df = Replace(post_df, teams, 'Comment')
-pre_df = Replace(pre_df, teams, 'Comment')
+post_df = Replace(post_df, teams, 'comment')
+pre_df = Replace(pre_df, teams, 'comment')
 
-pre_df.to_csv('./data/pre_soccer_replaced.csv')
-post_df.to_csv('./data/post_soccer_replaced.csv')
+pre_df.to_csv('./data/pre_soccer_replaced.csv',index=False)
+post_df.to_csv('./data/post_soccer_replaced.csv',index=False)
