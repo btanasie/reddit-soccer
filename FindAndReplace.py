ee1e7474e97953cb27a45850eabb1ff89d07e77b
Richard <rich.gao@hotmail.com>
2021-02-20 15:40:11 -0500

    Sentiment analysis per team and fixed timestamps for comments. Ran clean and tokenize + replace with emotions.

--- FindAndReplace.py
+++ FindAndReplace.py
@@ -3,16 +3,16 @@ import pandas as pd
 pre_df = pd.read_csv('./data/pre_soccer_extraction.csv')
 post_df = pd.read_csv('./data/post_soccer_extraction.csv')
 teams = pd.read_csv('./data/teams.csv')
+emotions = pd.read_csv('./data/emotions.csv')
 
 #post_df['comment'] = post_df['comment'].str.lower()
 pre_df['comment'] = pre_df['comment']
 pre_df['ptitle'] = pre_df['ptitle']
 teams['Other Name'] = teams['Other Name']
 
-
-def Replace(df, replace_list, key):
+def Replace(df, replace_list, key, columnname="Other Name"):
   #for i in range(0,len(df)):
-  sort = replace_list['Other Name'].str.len().sort_values().index
+  sort = replace_list[columnname].str.len().sort_values().index
   replace_list=replace_list.reindex(sort)
   for (main_value, to_replace) in replace_list.values:
     df[key].replace({to_replace: main_value}, regex=True,inplace=True)
@@ -24,6 +24,7 @@ def Replace(df, replace_list, key):
 post_df = Replace(post_df, teams, 'ptitle')
 pre_df = Replace(pre_df, teams, 'comment')
 pre_df = Replace(pre_df, teams, 'ptitle')
+pre_df = Replace(pre_df, emotions, 'comment',"words")
 
 pre_df.to_csv('./data/pre_soccer_replaced.csv',index=False)
 post_df.to_csv('./data/post_soccer_replaced.csv',index=False)
